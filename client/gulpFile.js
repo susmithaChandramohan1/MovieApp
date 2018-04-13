169399d6c95a5960962a34e453d050e4a3ea1c28
Susmitha Chandramohan (Digital) <SU352344@wipro.com>
2018-04-13 12:01:10 +0530

    Modified gulpFile and Added travis file

--- client/gulpFile.js
+++ client/gulpFile.js
@@ -3,6 +3,10 @@ var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 
+gulp.task('travis',['build','testServerJS'],function(){
+  process.exit(0);
+});
+
 gulp.task('browserify',function(){
    browserify('./src/js/main.js')
        .transform('reactify')
